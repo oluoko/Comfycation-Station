50519637545fe04fab44bde6ea683786c29ed4ea
oluoko <brianotieno586@gmail.com>
2024-10-15 14:40:40 +0300

    Comfycation Station: Form Schema in Zod

--- components/hotel/AddHotelForm.tsx
+++ components/hotel/AddHotelForm.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import * as z from "zod";
 import { Hotel, Room } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface AddHotelFormProps {
   hotel: HotelWithRooms | null;
@@ -10,7 +13,70 @@ export type HotelWithRooms = Hotel & {
   rooms: Room[];
 };
 
+const formSchema = z.object({
+  title: z.string().min(3, {
+    message: "Title must be at least 3 characters long",
+  }),
+  description: z.string().min(10, {
+    message: "Description must be at least 10 characters long",
+  }),
+  image: z.string().min(1, {
+    message: "Image is required",
+  }),
+  country: z.string().min(1, {
+    message: "Country is required",
+  }),
+
+  state: z.string().optional(),
+  city: z.string().optional(),
+  locationDescription: z.string().min(10, {
+    message: "Description must be at least 10 characters long",
+  }),
+  gym: z.boolean().optional(),
+  spa: z.boolean().optional(),
+  bar: z.boolean().optional(),
+  laundry: z.boolean().optional(),
+  restaurant: z.boolean().optional(),
+  shopping: z.boolean().optional(),
+  freeParking: z.boolean().optional(),
+  bikeRental: z.boolean().optional(),
+  freeWifi: z.boolean().optional(),
+  movieNights: z.boolean().optional(),
+  swimmingPool: z.boolean().optional(),
+  coffeeShop: z.boolean().optional(),
+  conferenceRoom: z.boolean().optional(),
+});
+
 const AddHotelForm = ({ hotel }: AddHotelFormProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      image: "",
+      country: "",
+      state: "",
+      city: "",
+      locationDescription: "",
+      gym: false,
+      spa: false,
+      bar: false,
+      laundry: false,
+      restaurant: false,
+      shopping: false,
+      freeParking: false,
+      bikeRental: false,
+      freeWifi: false,
+      movieNights: false,
+      swimmingPool: false,
+      coffeeShop: false,
+      conferenceRoom: false,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return <div>Add Hotel Form</div>;
 };
 
