082e6e53a4479d97b9d073f9e3ff8a7b9ea664f1
oluoko <brianotieno586@gmail.com>
2024-10-27 16:15:58 +0300

    Comfycation Station: DELETE route in app/api/hote/[hotelId]/route.ts

--- app/api/hotel/[hotelId]/route.ts
+++ app/api/hotel/[hotelId]/route.ts
@@ -33,3 +33,33 @@ export async function PATCH(
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { hotelId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!params.hotelId) {
+      return new NextResponse("Bad Request, Hotel ID is required", {
+        status: 400,
+      });
+    }
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const hotel = await prismadb.hotel.delete({
+      where: {
+        id: params.hotelId,
+      },
+    });
+
+    return NextResponse.json(hotel);
+  } catch (error) {
+    console.log("Error at /api/hotel/[hotelId]/route.ts DELETE: ", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
