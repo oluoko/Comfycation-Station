08b618d18ed69e6247f05c7d618f166203b17ce4
oluoko <brianotieno586@gmail.com>
2024-10-30 08:44:53 +0300

    Comfycation Station: AddRoomForm

--- components/room/AddRoomForm.tsx
+++ components/room/AddRoomForm.tsx
@@ -4,6 +4,18 @@ import { Hotel, Room } from "@prisma/client";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+import { Checkbox } from "../ui/checkbox";
 
 interface AddRoomFormProps {
   hotel?: Hotel & {
@@ -68,7 +80,73 @@ const AddRoomForm = ({ hotel, room, handleDialogOpen }: AddRoomFormProps) => {
       soundProofed: false,
     },
   });
-  return <div className="">Add Room Form</div>;
+  return (
+    <div className="max-h[75vh] overflow-y-auto px-2">
+      <Form {...form}>
+        <form className="space-y-6">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Room Title <span className="text-2xl text-red-600">*</span>
+                </FormLabel>
+                <FormDescription>Provide a room name.</FormDescription>
+                <FormControl>
+                  <Input placeholder="Double Room" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Description <span className="text-2xl text-red-600">*</span>
+                </FormLabel>
+                <FormDescription>
+                  Provide a description for the room.
+                </FormDescription>
+                <FormControl>
+                  <Textarea placeholder="Room description" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="">
+            <FormLabel>Choose Room Amenities</FormLabel>
+            <FormDescription>
+              What makes this room a great choice
+            </FormDescription>
+            <div className="grid grid-cols-2 gap-2 mt-2">
+              <FormField
+                control={form.control}
+                name="roomService"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-end space-x-3 rounded-md border p-4">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                      <FormLabel>24hrs Room Service</FormLabel>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default AddRoomForm;
