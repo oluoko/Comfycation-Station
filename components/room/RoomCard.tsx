f7badf91dbddd6a454450dd7ac0a35eff863f5da
oluoko <brianotieno586@gmail.com>
2024-10-30 17:58:38 +0300

    Comfycation Station: Room Card

--- components/room/RoomCard.tsx
+++ components/room/RoomCard.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import { Booking, Hotel, Room } from "@prisma/client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import Image from "next/image";
 
 interface RoomCardProps {
   hotel?: Hotel & {
@@ -11,7 +19,24 @@ interface RoomCardProps {
 }
 
 const RoomCard = ({ hotel, room, bookings = [] }: RoomCardProps) => {
-  return <div>{room.title}</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{room.title}</CardTitle>
+        <CardDescription>{room.description}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <div className="aspect-square overflow-hidden relative h-[200px] rounded-lg">
+          <Image
+            fill
+            src={room.image}
+            alt={room.title}
+            className="object-cover"
+          />
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default RoomCard;
