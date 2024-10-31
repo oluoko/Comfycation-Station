669bab2c7c5b050169b6ca26b85223a856593343
oluoko <brianotieno586@gmail.com>
2024-10-31 04:03:17 +0300

    Comfycation Station: getHotels actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import { getHotels } from "@/actions/getHotels";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+interface HomeProps {
+  searchParams: {
+    title: string;
+    country: string;
+    state: string;
+    city: string;
+  };
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const hotels = await getHotels(searchParams);
+
+  if (!hotels) return <div>There are no hotels</div>;
   return <div className="">Home Page</div>;
 }
